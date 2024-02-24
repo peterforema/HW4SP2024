c9e386b855a47d4fa8a8038dbea8ab72e98d1845
peterforema <peter.foreman@okstate.edu>
2024-02-23 18:07:26 -0600

    2/23 update

--- HW4b.py
+++ HW4b.py
@@ -1,15 +1,27 @@
-from scipy.optimize import fsolve
+#region imports
 import numpy as np
+from scipy.optimize import fsolve
+#endregion
 
-def equations(x):
-    eq1 = x[0] - 3 * np.cos(x[0])
-    eq2 = np.cos(2*x[0]) * (x[0]**3) - 3
-    return [eq1, eq2]
+def equation1(x):
+    '''equation from homework 4'''
+    return x - 3 * np.cos(x)
 
+def equation2(x):
+    '''''equation from homework 4'''
+    return np.cos(2*x) * x**3
 
-initial_guess = [0, 1]                      #guess is for the roots of the equations
-roots = fsolve(equations, initial_guess)       # Solve the equations
+# Find roots of equation 1
+roots1 = fsolve(equation1, x0=[0, 2, 4])
+print("Roots of equation 1:", roots1)
 
-print("Roots of the equations:")
-print("x - 3*cos(x) = 0:", roots[0])
-print("cos(2x)*(x^3) = 3:", roots[1])
+# Find roots of equation 2
+roots2 = fsolve(equation2, x0=[0, 2, 4])
+print("Roots of equation 2:", roots2)
+
+# Check if functions intersect
+intersections = fsolve(lambda x: equation1(x) - equation2(x), x0=[0, 2, 4])
+if len(intersections) > 0:
+    print("Functions intersect at x =", intersections)
+else:
+    print("Functions do not intersect.")
---
+++ HW4c.py
@@ -0,0 +1,35 @@
+#region imports
+import numpy as np
+from scipy.linalg import solve
+#endregion
+
+
+def matrixsolve(A_aug):
+    '''function creates augmented matrix and populates with samples from problem c'''
+    A = A_aug[:, :-1]
+    b = A_aug[:, -1]
+    return solve(A, b)
+
+Aaug1 = np.array([          #1st matrix
+    [3, 1, -1, 2],
+    [1, 4, 1, 12],
+    [2, 1, 2, 10]
+])
+
+Aaug2 = np.array([          #2nd matrix
+    [3, 1, 4, 12, 12],
+    [1, -10, 2, 4, 2],
+    [-1, 2, 7, 3, 37],
+    [9, 2, 3, 4, 21]
+])
+
+
+
+print("Problem 1:")                 #problem 1 print statements
+for i, sol in enumerate(matrixsolve(Aaug1)):
+    print(f"x{i+1} =", sol)
+
+
+print("\nProblem 2:")               #problem 2 print statements
+for i, sol in enumerate(matrixsolve(Aaug2)):
+    print(f"x{i+1} =", sol)
